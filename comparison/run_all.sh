cc1e6a45224b616cd7129ca1b8472da8bdc5e32a
Martin Ingram <martin.ingram@konux.de>
2022-10-02 17:36:18 +0200

    Add code

---
+++ comparison/run_all.sh
@@ -0,0 +1,13 @@
+# MODEL_NAME=potus
+
+for MODEL_NAME in occ_det potus tennis microcredit; do
+
+	# TODO: Add SADVI, full rank SADVI
+	# python fit_raabbvi.py "$MODEL_NAME"
+	# python fit_dadvi.py "$MODEL_NAME"
+	# python fit_pymc_sadvi.py "$MODEL_NAME" advi
+	# python fit_pymc_sadvi.py "$MODEL_NAME" fullrank_advi
+	python fit_mcmc.py "$MODEL_NAME"
+	# python fit_dadvi_lrvb.py "$MODEL_NAME"
+
+done
