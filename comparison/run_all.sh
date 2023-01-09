973beb92fe3164b6ff90f6f1a7615b1136750e38
Martin Ingram <martin.ingram@gmail.com>
2023-01-09 07:47:34 +0100

    Fix call to LRVB

--- comparison/run_all.sh
+++ comparison/run_all.sh
@@ -1,16 +1,18 @@
-TARGET_DIR='/media/martin/External Drive/projects/lrvb_paper/dadvi_runs_october_2022'
+TARGET_DIR='/media/martin/External Drive/projects/lrvb_paper/dadvi_runs_january_2023'
 COVERAGE_TARGET_DIR='/media/martin/External Drive/projects/lrvb_paper/coverage_warm_starts_rerun'
 
 for MODEL_NAME in microcredit occ_det tennis potus; do
 
-	python fit_raabbvi.py "$MODEl_NAME" "$TARGET_DIR"
-	python fit_dadvi.py "$MODEl_NAME" "$TARGET_DIR"
-	python fit_pymc_sadvi.py "$MODEl_NAME" "$TARGET_DIR" advi
-	python fit_pymc_sadvi.py "$MODEl_NAME" "$TARGET_DIR" fullrank_advi
-	python fit_mcmc.py "$MODEl_NAME" "$TARGET_DIR"
-	python fit_dadvi_lrvb.py "$MODEl_NAME" "$TARGET_DIR"
-	python fit_doubling_dadvi_lrvb.py "$MODEl_NAME" "$TARGET_DIR"
-        python fit_dadvi_lrvb.py "$MODEl_NAME" "$TARGET_DIR" "$TARGET_DIR" CG
+	# python fit_raabbvi.py "$MODEL_NAME" "$TARGET_DIR"
+	# python fit_dadvi.py "$MODEL_NAME" "$TARGET_DIR"
+	# python fit_pymc_sadvi.py "$MODEL_NAME" "$TARGET_DIR" advi
+	# python fit_pymc_sadvi.py "$MODEL_NAME" "$TARGET_DIR" fullrank_advi
+	# python fit_mcmc.py "$MODEL_NAME" "$TARGET_DIR"
+
+	python fit_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" Direct
+        python fit_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" CG
+
+	# python fit_doubling_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" 0.25
 
         # Run coverage
 	# for min_m_power in 3 4 5 6; do
--- comparison/run_all_arm.sh
+++ comparison/run_all_arm.sh
@@ -1,17 +1,18 @@
-TARGET_DIR='/media/martin/External Drive/projects/lrvb_paper/dadvi_runs_october_2022'
+TARGET_DIR='/media/martin/External Drive/projects/lrvb_paper/dadvi_runs_january_2023'
 COVERAGE_TARGET_DIR='/media/martin/External Drive/projects/lrvb_paper/coverage_warm_starts_rerun'
 
 while read MODEL_NAME; do
     echo "$MODEL_NAME"
 
     # Run inference
-    python fit_pymc_sadvi.py "$MODEL_NAME" "$TARGET_DIR" advi
-    python fit_pymc_sadvi.py "$MODEL_NAME" "$TARGET_DIR" fullrank_advi
-    python fit_raabbvi.py "$MODEL_NAME" "$TARGET_DIR"
-    python fit_dadvi.py "$MODEL_NAME" "$TARGET_DIR"
-    python fit_mcmc.py "$MODEL_NAME" "$TARGET_DIR"
-    python fit_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" "$TARGET_DIR" CG
-    python fit_doubling_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" 0.25
+    # python fit_pymc_sadvi.py "$MODEL_NAME" "$TARGET_DIR" advi
+    # python fit_pymc_sadvi.py "$MODEL_NAME" "$TARGET_DIR" fullrank_advi
+    # python fit_raabbvi.py "$MODEL_NAME" "$TARGET_DIR"
+    # python fit_dadvi.py "$MODEL_NAME" "$TARGET_DIR"
+    # python fit_mcmc.py "$MODEL_NAME" "$TARGET_DIR"
+    python fit_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" CG
+    python fit_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" Direct
+    # python fit_doubling_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" 0.25
 
     # Run coverage
     # for min_m_power in 3 4 5 6; do
