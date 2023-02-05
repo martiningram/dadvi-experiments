d7d0a4167d4e7ceb9a95c051535096b1fb670aa4
Martin Ingram <martin.ingram@konux.de>
2023-02-05 08:38:01 +0100

    Fix up coverage script and add names

--- comparison/run_all.sh
+++ comparison/run_all.sh
@@ -1,7 +1,9 @@
 TARGET_DIR='/media/martin/External Drive/projects/lrvb_paper/dadvi_runs_january_2023'
-COVERAGE_TARGET_DIR='/media/martin/External Drive/projects/lrvb_paper/coverage_warm_starts_rerun'
+# COVERAGE_TARGET_DIR='/media/martin/External Drive/projects/lrvb_paper/coverage_warm_starts_rerun'
+COVERAGE_TARGET_DIR='/tmp/bla/'
 
-for MODEL_NAME in microcredit occ_det tennis potus; do
+# for MODEL_NAME in microcredit occ_det tennis potus; do
+for MODEL_NAME in kidscore_momiq; do
 
 	# python fit_raabbvi.py "$MODEL_NAME" "$TARGET_DIR"
 	# python fit_dadvi.py "$MODEL_NAME" "$TARGET_DIR"
@@ -9,21 +11,21 @@ for MODEL_NAME in microcredit occ_det tennis potus; do
 	# python fit_pymc_sadvi.py "$MODEL_NAME" "$TARGET_DIR" fullrank_advi
 	# python fit_mcmc.py "$MODEL_NAME" "$TARGET_DIR"
 
-	python fit_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" Direct
-        python fit_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" CG
+	# python fit_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" Direct
+        # python fit_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" CG
 
 	# python fit_doubling_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" 0.25
 
         # Run coverage
-	# for min_m_power in 3 4 5 6; do
+	for min_m_power in 3 4 5 6; do
 
-	# 	python run_multiple_dadvi.py \
-	# 		--model-name "$MODEL_NAME" \
-	# 		--target-dir "$COVERAGE_TARGET_DIR" \
-	# 		--min-m-power $min_m_power \
-	# 		--n-reruns 10 \
-	# 		--warm-start
+		python run_multiple_dadvi.py \
+			--model-name "$MODEL_NAME" \
+			--target-dir "$COVERAGE_TARGET_DIR" \
+			--min-m-power $min_m_power \
+			--n-reruns 10 \
+			--warm-start
 
-	# done;
+	done;
 
 done
