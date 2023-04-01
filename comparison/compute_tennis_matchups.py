790a92a0ddb1f769a723c776cc2daed0f96a3c85
Martin Ingram <martin.ingram@gmail.com>
2023-04-01 20:58:15 +0000

    Update and add potus

--- comparison/compute_tennis_matchups.py
+++ comparison/compute_tennis_matchups.py
@@ -4,6 +4,7 @@ import numpy as np
 from functools import partial
 from collections import defaultdict
 from glob import glob
+from time import time
 
 from utils import fetch_tennis_model
 from config import SACKMANN_DIR
@@ -11,6 +12,7 @@ from dadvi.pymc.jax_api import DADVIResult
 from dadvi.pymc.pymc_to_jax import get_jax_functions_from_pymc
 from dadvi.jax import build_dadvi_funs
 from jax import vmap
+from utils import get_run_datetime_and_hostname
 
 
 ### TENNIS
@@ -61,15 +63,23 @@ pairs = [*zip(p1_choices, p2_choices)]
 lrvb_results = dict()
 draw_results = defaultdict(dict)
 
+total_runtime = 0.
+total_hvp = 0
+
 for cur_p1, cur_p2 in pairs:
 
     p1_id, p2_id = encoder.transform([cur_p1, cur_p2])
 
+    cur_start_time = time()
     lrvb_differences = (
         dadvi_res.get_frequentist_sd_and_lrvb_correction_of_scalar_valued_function(
             partial(skill_difference, p1_id=p1_id, p2_id=p2_id)
         )
     )
+    cur_end_time = time()
+    total_runtime += (cur_end_time - cur_start_time)
+    total_hvp += lrvb_differences['n_hvp_calls']
+
     lrvb_results[f"{cur_p1} vs {cur_p2}"] = lrvb_differences
 
     # Add the chain dimension on
@@ -83,14 +93,17 @@ for cur_p1, cur_p2 in pairs:
 
     for cur_file in draw_files:
 
+        short_name = "_".join(cur_file.split("/")[-3].split("_")[:-1])
+
+        if short_name == 'lrvb_cg':
+            continue
+
         cur_loaded = np.load(cur_file)
 
         chain_dim = cur_loaded[list(cur_loaded.keys())[0]].shape[0]
         cur_result = compute_distribution_from_draws(
             cur_loaded, partial(skill_difference, p1_id=p1_id, p2_id=p2_id)
         )
-
-        short_name = "_".join(cur_file.split("/")[-3].split("_")[:-1])
 
         draw_results[f"{cur_p1} vs {cur_p2}"][short_name] = cur_result.reshape(
             chain_dim, -1
@@ -109,6 +122,9 @@ methods_stacked = {x: np.stack(y, axis=-1) for x, y in methods_stacked.items()}
 # Add these to the draw dicts
 for cur_file in draw_files:
 
+    if short_name == 'lrvb_cg':
+        continue
+
     cur_loaded = dict(np.load(cur_file))
     short_name = "_".join(cur_file.split("/")[-3].split("_")[:-1])
     cur_loaded["match_predictions"] = methods_stacked[short_name]
@@ -126,3 +142,21 @@ os.makedirs(target_dir, exist_ok=True)
 np.savez(
     os.path.join(target_dir, "tennis.npz"), match_predictions=methods_stacked["lrvb_cg"]
 )
+
+# Save the runtime etc also
+runtime_cost = {
+        'lrvb_hvp_calls': total_hvp,
+        'lrvb_runtime': total_runtime,
+        **get_run_datetime_and_hostname()
+}
+
+target_folder = (
+    f"{EXPERIMENT_BASE_DIR}/lrvb_cg_results/lrvb_cg_info/"
+)
+
+os.makedirs(target_folder, exist_ok=True)
+
+target_file = os.path.join(target_folder, f"tennis.pkl")
+
+with open(target_file, "wb") as f:
+    pickle.dump(runtime_cost, f)
