a139473b7015a39220c0946472c48f2f6f103ea8
Martin Ingram <martin.ingram@konux.de>
2023-04-19 07:43:47 +0200

    Add summary for tennis coverage

--- comparison/compute_tennis_matchups.py
+++ comparison/compute_tennis_matchups.py
@@ -29,100 +29,103 @@ def compute_distribution_from_draws(draws, function):
     return results
 
 
-EXPERIMENT_BASE_DIR = '/home/martin.ingram/experiment_runs/march_2023'
-TENNIS_PICKLE_FILE = "/home/martin.ingram/experiment_runs/march_2023/dadvi_results/dadvi_info/tennis.pkl"
-tennis_res = pickle.load(open(TENNIS_PICKLE_FILE, "rb"))
-tennis_model = fetch_tennis_model(1969, sackmann_dir=SACKMANN_DIR)
-model = tennis_model["model"]
-jax_funs = get_jax_functions_from_pymc(model)
-dadvi_funs = build_dadvi_funs(jax_funs["log_posterior_fun"])
-encoder = tennis_model["encoder"]
-
-dadvi_res = DADVIResult(
-    tennis_res["fixed_draws"],
-    tennis_res["opt_result"]["opt_result"].x,
-    jax_funs["unflatten_fun"],
-    dadvi_funs=dadvi_funs,
-)
-
-np.random.seed(2)
-p1_choices = np.random.choice(encoder.classes_, size=20, replace=False)
-p2_choices = np.random.choice(
-    [x for x in encoder.classes_ if x not in p1_choices], size=20, replace=False
-)
+if __name__ == '__main__':
 
 
-draw_files = glob(
-   f"{EXPERIMENT_BASE_DIR}/*/draw_dicts/tennis.npz"
-)
+    EXPERIMENT_BASE_DIR = '/home/martin.ingram/experiment_runs/march_2023'
+    TENNIS_PICKLE_FILE = "/home/martin.ingram/experiment_runs/march_2023/dadvi_results/dadvi_info/tennis.pkl"
+    tennis_res = pickle.load(open(TENNIS_PICKLE_FILE, "rb"))
+    tennis_model = fetch_tennis_model(1969, sackmann_dir=SACKMANN_DIR)
+    model = tennis_model["model"]
+    jax_funs = get_jax_functions_from_pymc(model)
+    dadvi_funs = build_dadvi_funs(jax_funs["log_posterior_fun"])
+    encoder = tennis_model["encoder"]
 
-pairs = [*zip(p1_choices, p2_choices)]
-
-lrvb_results = dict()
-draw_results = defaultdict(dict)
-
-for cur_p1, cur_p2 in pairs:
-
-    p1_id, p2_id = encoder.transform([cur_p1, cur_p2])
-
-    lrvb_differences = (
-        dadvi_res.get_frequentist_sd_and_lrvb_correction_of_scalar_valued_function(
-            partial(skill_difference, p1_id=p1_id, p2_id=p2_id)
-        )
+    dadvi_res = DADVIResult(
+        tennis_res["fixed_draws"],
+        tennis_res["opt_result"]["opt_result"].x,
+        jax_funs["unflatten_fun"],
+        dadvi_funs=dadvi_funs,
     )
-    lrvb_results[f"{cur_p1} vs {cur_p2}"] = lrvb_differences
 
-    # Add the chain dimension on
-    draw_results[f"{cur_p1} vs {cur_p2}"]["lrvb_cg"] = np.random.normal(
-        loc=lrvb_differences["mean"], scale=lrvb_differences["lrvb_sd"], size=1000
-    ).reshape(1, -1)
+    np.random.seed(2)
+    p1_choices = np.random.choice(encoder.classes_, size=20, replace=False)
+    p2_choices = np.random.choice(
+        [x for x in encoder.classes_ if x not in p1_choices], size=20, replace=False
+    )
 
-for cur_p1, cur_p2 in pairs:
 
-    p1_id, p2_id = encoder.transform([cur_p1, cur_p2])
+    draw_files = glob(
+       f"{EXPERIMENT_BASE_DIR}/*/draw_dicts/tennis.npz"
+    )
 
+    pairs = [*zip(p1_choices, p2_choices)]
+
+    lrvb_results = dict()
+    draw_results = defaultdict(dict)
+
+    for cur_p1, cur_p2 in pairs:
+
+        p1_id, p2_id = encoder.transform([cur_p1, cur_p2])
+
+        lrvb_differences = (
+            dadvi_res.get_frequentist_sd_and_lrvb_correction_of_scalar_valued_function(
+                partial(skill_difference, p1_id=p1_id, p2_id=p2_id)
+            )
+        )
+        lrvb_results[f"{cur_p1} vs {cur_p2}"] = lrvb_differences
+
+        # Add the chain dimension on
+        draw_results[f"{cur_p1} vs {cur_p2}"]["lrvb_cg"] = np.random.normal(
+            loc=lrvb_differences["mean"], scale=lrvb_differences["lrvb_sd"], size=1000
+        ).reshape(1, -1)
+
+    for cur_p1, cur_p2 in pairs:
+
+        p1_id, p2_id = encoder.transform([cur_p1, cur_p2])
+
+        for cur_file in draw_files:
+
+            cur_loaded = np.load(cur_file)
+
+            chain_dim = cur_loaded[list(cur_loaded.keys())[0]].shape[0]
+            cur_result = compute_distribution_from_draws(
+                cur_loaded, partial(skill_difference, p1_id=p1_id, p2_id=p2_id)
+            )
+
+            short_name = "_".join(cur_file.split("/")[-3].split("_")[:-1])
+
+            draw_results[f"{cur_p1} vs {cur_p2}"][short_name] = cur_result.reshape(
+                chain_dim, -1
+            )
+
+    # Now we have all the draw results together. Add them to the npzs.
+    # Maybe let's just flatten them to make it easier.
+    methods_stacked = defaultdict(list)
+
+    for cur_matchup, cur_method_lookup in draw_results.items():
+        for cur_method, cur_draws in cur_method_lookup.items():
+            methods_stacked[cur_method].append(cur_draws)
+
+    methods_stacked = {x: np.stack(y, axis=-1) for x, y in methods_stacked.items()}
+
+    # Add these to the draw dicts
     for cur_file in draw_files:
 
-        cur_loaded = np.load(cur_file)
-
-        chain_dim = cur_loaded[list(cur_loaded.keys())[0]].shape[0]
-        cur_result = compute_distribution_from_draws(
-            cur_loaded, partial(skill_difference, p1_id=p1_id, p2_id=p2_id)
-        )
-
+        cur_loaded = dict(np.load(cur_file))
         short_name = "_".join(cur_file.split("/")[-3].split("_")[:-1])
+        cur_loaded["match_predictions"] = methods_stacked[short_name]
 
-        draw_results[f"{cur_p1} vs {cur_p2}"][short_name] = cur_result.reshape(
-            chain_dim, -1
-        )
+        # Update with new stuff
+        np.savez(cur_file, **cur_loaded)
 
-# Now we have all the draw results together. Add them to the npzs.
-# Maybe let's just flatten them to make it easier.
-methods_stacked = defaultdict(list)
+    # Make a final one for lrvb_cg
+    target_dir = (
+        f"{EXPERIMENT_BASE_DIR}/lrvb_cg_results/draw_dicts/"
+    )
 
-for cur_matchup, cur_method_lookup in draw_results.items():
-    for cur_method, cur_draws in cur_method_lookup.items():
-        methods_stacked[cur_method].append(cur_draws)
+    os.makedirs(target_dir, exist_ok=True)
 
-methods_stacked = {x: np.stack(y, axis=-1) for x, y in methods_stacked.items()}
-
-# Add these to the draw dicts
-for cur_file in draw_files:
-
-    cur_loaded = dict(np.load(cur_file))
-    short_name = "_".join(cur_file.split("/")[-3].split("_")[:-1])
-    cur_loaded["match_predictions"] = methods_stacked[short_name]
-
-    # Update with new stuff
-    np.savez(cur_file, **cur_loaded)
-
-# Make a final one for lrvb_cg
-target_dir = (
-    f"{EXPERIMENT_BASE_DIR}/lrvb_cg_results/draw_dicts/"
-)
-
-os.makedirs(target_dir, exist_ok=True)
-
-np.savez(
-    os.path.join(target_dir, "tennis.npz"), match_predictions=methods_stacked["lrvb_cg"]
-)
+    np.savez(
+        os.path.join(target_dir, "tennis.npz"), match_predictions=methods_stacked["lrvb_cg"]
+    )
