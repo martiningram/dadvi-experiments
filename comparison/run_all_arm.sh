37ee47f1151cbb78766311469c43da7a8f711ef7
Martin Ingram <martin.ingram@konux.de>
2023-11-21 20:46:35 +0100

    Improve some of the bash script

--- comparison/run_all_arm.sh
+++ comparison/run_all_arm.sh
@@ -1,27 +1,42 @@
-TARGET_DIR='/home/martin.ingram/experiment_runs/march_2023'
-COVERAGE_TARGET_DIR='/media/martin/External Drive/projects/lrvb_paper/coverage_warm_starts_rerun'
+# This script runs the experiments for the ARM models
+TARGET_DIR='./experiment_runs/november_2023'
+COVERAGE_TARGET_DIR='./experiment_runs/coverage/'
 
 while read MODEL_NAME; do
     echo "$MODEL_NAME"
 
-    # Run inference
+    echo "Running doubling DADVI"
     python fit_doubling_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" 0.25
-    # python fit_raabbvi.py "$MODEL_NAME" "$TARGET_DIR"
+
+    echo "Running DADVI"
     python fit_dadvi.py "$MODEL_NAME" "$TARGET_DIR"
+
+    echo "Running MCMC"
     python fit_mcmc.py "$MODEL_NAME" "$TARGET_DIR"
+
+    echo "Running DADVI with Direct LRVB"
     python fit_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" Direct
 
+    echo "Running SADVI with mean field"
     python fit_pymc_sadvi.py "$MODEL_NAME" "$TARGET_DIR" advi
+
+    echo "Running SADVI with full rank"
     python fit_pymc_sadvi.py "$MODEL_NAME" "$TARGET_DIR" fullrank_advi
 
+    echo "Running coverage experiments"
     # Run coverage
-    # for min_m_power in 3 4 5 6; do
-    #         python run_multiple_dadvi.py \
-    #     	--model-name "$MODEL_NAME" \
-    #     	--target-dir "$COVERAGE_TARGET_DIR" \
-    #     	--min-m-power $min_m_power \
-    #     	--n-reruns 100 \
-    #     	--warm-start
-    # done;
+    for min_m_power in 3 4 5 6; do
+            python run_multiple_dadvi.py \
+        	--model-name "$MODEL_NAME" \
+        	--target-dir "$COVERAGE_TARGET_DIR" \
+        	--min-m-power $min_m_power \
+        	--n-reruns 100 \
+        	--warm-start
+    done;
+
+    echo "Running RAABBVI"
+    python fit_raabbvi.py "$MODEL_NAME" "$TARGET_DIR"
+
+    break
 
 done < all_arm_names.txt
