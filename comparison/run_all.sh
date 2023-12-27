f99deebec7bc1f9cd920dfedeb084a26c2ab66e5
Martin Ingram <martin.ingram@konux.de>
2023-12-27 10:27:51 +0100

    Start tidying

--- comparison/run_all.sh
+++ comparison/run_all.sh
@@ -1,36 +1,35 @@
-TARGET_DIR='/home/martin.ingram/experiment_runs/march_2023/'
-COVERAGE_TARGET_DIR='/home/martin.ingram/experiment_runs/march_2023_coverage/'
+TARGET_DIR='./experiment_runs/november_2023/'
+COVERAGE_TARGET_DIR='./experiment_runs/november_2023_coverage/'
 
-# for MODEL_NAME in microcredit occ_det tennis potus; do
-for MODEL_NAME in tennis occ_det potus; do
+for MODEL_NAME in microcredit occ_det tennis potus; do
 
         echo "$MODEL_NAME"
 
-	# echo "Running DADVI"
-	# python fit_dadvi.py "$MODEL_NAME" "$TARGET_DIR"
+	echo "Running DADVI"
+	python fit_dadvi.py "$MODEL_NAME" "$TARGET_DIR"
 
-	# echo "Running NUTS"
-	# python fit_mcmc.py "$MODEL_NAME" "$TARGET_DIR"
+	echo "Running NUTS"
+	python fit_mcmc.py "$MODEL_NAME" "$TARGET_DIR"
 
-	# echo "Running SADVI mean field"
-	# python fit_pymc_sadvi.py "$MODEL_NAME" "$TARGET_DIR" advi
+	echo "Running SADVI mean field"
+	python fit_pymc_sadvi.py "$MODEL_NAME" "$TARGET_DIR" advi
 
-	# echo "Running RAABBVI"
-	# python fit_raabbvi.py "$MODEL_NAME" "$TARGET_DIR"
+	echo "Running RAABBVI"
+	python fit_raabbvi.py "$MODEL_NAME" "$TARGET_DIR"
 
-	# if [ $MODEL_NAME != 'potus' ]
-	# then
-		# echo "Running bigger models also."
+	if [ $MODEL_NAME != 'potus' ]
+	then
+		echo "Running bigger models also."
 
-		# echo "Running doubling DADVI"
-		# python fit_doubling_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" 0.25
+		echo "Running doubling DADVI"
+		python fit_doubling_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" 0.25
 
-		# echo "Running LRVB Direct"
-		# python fit_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" Direct
+		echo "Running LRVB Direct"
+		python fit_dadvi_lrvb.py "$MODEL_NAME" "$TARGET_DIR" Direct
 
-		# echo "Running PyMC SADVI Full rank"
-		# python fit_pymc_sadvi.py "$MODEL_NAME" "$TARGET_DIR" fullrank_advi
-	# fi
+		echo "Running PyMC SADVI Full rank"
+		python fit_pymc_sadvi.py "$MODEL_NAME" "$TARGET_DIR" fullrank_advi
+	fi
 
         # Run coverage
 	for M in 8 16 32 64; do
