7be77d8762519e1f7df0539356d9c5d4d2ce8a79
Ryan Giordano <rgiordan@gmail.com>
2024-01-09 13:36:57 -0800

    make scripts executable

--- comparison/run_and_log.sh
+++
@@ -1,6 +0,0 @@
-TARGET_DIR='/home/martin.ingram/experiment_runs/december_2023/run_log'
-
-mkdir -p "$TARGET_DIR"
-
-bash run_all.sh 2>&1 | tee -a "$TARGET_DIR"/tennis_run.log
-# bash run_all_arm.sh 2>&1 | tee -a "$TARGET_DIR"/run_arm.log
